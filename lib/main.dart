0ab587d4fa1e3fb6defd946bb70e7819485ce83e
valcons97 <jonathanadi46@gmail.com>
2022-04-01 10:19:15 +0700

    feat: Converting into Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jo_todos/bloc/todo_bloc.dart';
 import 'package:jo_todos/home/stream_builder.dart';
-import 'package:jo_todos/services/memory_task_creator.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root
-  // of your application.
-
   @override
   Widget build(BuildContext context) {
-    final MemoryTaskCreator taskCreator = MemoryTaskCreator();
-    return MaterialApp(
-      title: "To-Do List",
-      home: StreamBuilderPage(
-        taskCreator: taskCreator,
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => TodoBloc()..add(LoadTodoList()))
+        ],
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'To-Do List',
+          home: StreamBuilderPage(),
+        ));
   }
 }
--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -2,12 +2,10 @@ class Todo {
   Todo({
     required this.name,
     required this.date,
-    this.checked = false,
   });
 
   late String name;
   final DateTime date;
-  bool checked;
 
   @override
   String toString() {
